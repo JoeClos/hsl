ed9d2f2a7e876e890eab1eb442aa033de03ef18d
Josephine Closan <89244648+JoeClos@users.noreply.github.com>
2023-03-30 13:49:46 +0300

    display/transform journeys distance to km and min

--- hsl-client/src/components/Journeys.jsx
+++ hsl-client/src/components/Journeys.jsx
@@ -1,43 +1,46 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react';
-import {api} from '../config';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { api } from "../config";
 
-const Journeys = ()=> {
-    const [journeys, setJourneys] = useState([]);
+const Journeys = () => {
+  const [journeys, setJourneys] = useState([]);
 
-    useEffect(() => {
-        const getJourneys = api + '/journeys';
-        axios
-            .get(getJourneys)
-            .then((response) => {
-                setJourneys(response)
-                console.log(response)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    }, [])
+  useEffect(() => {
+    const getJourneys = api + "/journeys";
+    axios
+      .get(getJourneys)
+      .then((response) => {
+        setJourneys(response);
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    return(
-        <div>
-            {journeys.data && journeys.data.map((journey) => (
-                <div  key={journey._id}>
-                    <table>
-                        <tr>
-                            <th>Departure</th>
-                            <th>Return</th>
-                            <th>Covered disance (km)</th>
-                            <th>Duration (min)</th>
-                        </tr>
-                        <td>{journey.departure_station_name}</td>
-                        <td>{journey.return_station_name}</td>
-                        <td>{(journey.covered_distance / 1000).toFixed(1)} km</td>
-                        <td>{(journey.duration / 60).toFixed()} min</td>
-                    </table>
-                </div>
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Departure</th>
+          <th>Return</th>
+          <th>Covered disance (km)</th>
+          <th>Duration (min)</th>
+        </tr>
+        <tbody>
+          {journeys.data &&
+            journeys.data.map((journey) => (
+              <tr key={journey._id}>
+                <td>{journey.departure_station_name}</td>
+                <td>{journey.return_station_name}</td>
+                <td>{(journey.covered_distance / 1000).toFixed(1)} km</td>
+                <td>{(journey.duration / 60).toFixed()} min</td>
+              </tr>
             ))}
-        </div>
-    )
-}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Journeys;
