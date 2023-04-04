1e6eca7b4854c7faae445aed2e0c0dab7622446d
Josephine Closan <89244648+JoeClos@users.noreply.github.com>
2023-04-04 12:22:36 +0300

    implement pagination

--- hsl-client/src/components/Stations.jsx
+++ hsl-client/src/components/Stations.jsx
@@ -1,10 +1,47 @@
 import axios from "axios";
-import { useEffect, useState   } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../config";
 import { Link } from "react-router-dom";
+import {
+  Paper,
+  Table,
+  TablePagination,
+  TableContainer,
+  TableRow,
+  TableHead,
+  TableCell,
+  TableBody,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import { tableCellClasses } from "@mui/material/TableCell";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import ReadMoreOutlinedIcon from "@mui/icons-material/ReadMoreOutlined";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import { styled } from "@mui/material/styles";
 
-const Stations = ({station}) => {
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+}));
+
+const Stations = ({ station }) => {
   const [stations, setStations] = useState([]);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [page, setPage] = useState(0);
+
+  const handleChangePage = (_event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   useEffect(() => {
     const getJourneys = api + "/stations";
@@ -18,41 +55,83 @@ const Stations = ({station}) => {
         console.log(err);
       });
   }, []);
+
   return (
     <div>
-      <table>
-        <tbody>
-          <tr>
-            <th>Station name</th>
-            <th>Address</th>
-            <th>City</th>
-            <th>Operator</th>
-            <th>Capacity</th>
-            <th>Coordinates</th>
-            <th></th>
-          </tr>
-          {stations.data &&
-            stations.data.map((station) => (
-              <tr key={station._id}>
-                <td>{station.name}</td>
-                <td>{station.osoite}</td>
-                <td>{station.kaupunki}</td>
-                <td>{station.operaattor}</td>
-                <td>{station.kapasiteet}</td>
-                <td>
-                  <ul>
-                    <li>Lat: {station.x}</li>
-                    <li>Long: {station.y}</li>
-                  </ul>
-                </td>
-                <td>
-                  <Link  to={`/stations/${station._id}`}>More</Link>
-                </td>
-
-              </tr>
-            ))}
-        </tbody>
-      </table>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 50, 100]}
+        component="div"
+        count={(stations.data || []).length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+      <Paper>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Station name</StyledTableCell>
+                <StyledTableCell>Address</StyledTableCell>
+                <StyledTableCell>City</StyledTableCell>
+                <StyledTableCell>Operator</StyledTableCell>
+                <StyledTableCell>Capacity</StyledTableCell>
+                <StyledTableCell>Coordinates</StyledTableCell>
+                <StyledTableCell></StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {stations.data &&
+                stations.data
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((station) => (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                      key={station._id}
+                    >
+                      <TableCell>{station.name}</TableCell>
+                      <TableCell>{station.osoite}</TableCell>
+                      <TableCell>{station.kaupunki}</TableCell>
+                      <TableCell>{station.operaattor}</TableCell>
+                      <TableCell>{station.kapasiteet}</TableCell>
+                      <TableCell>
+                        <List>
+                          <ListItem>
+                            <ListItemAvatar>
+                              <Avatar>
+                                <LocationOnOutlinedIcon />
+                              </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Lat" secondary={station.x} />
+                          </ListItem>
+                          <ListItem>
+                            <ListItemAvatar>
+                              <Avatar>
+                                <LocationOnOutlinedIcon />
+                              </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText
+                              primary="Long"
+                              secondary={station.y}
+                            />
+                          </ListItem>
+                        </List>
+                      </TableCell>
+                      <TableCell>
+                        <Link to={`/stations/${station._id}`}>
+                          <ReadMoreOutlinedIcon />
+                        </Link>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </div>
   );
 };
