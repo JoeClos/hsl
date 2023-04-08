caa987305134d9f516b7b49159d13dcf0d4bff63
Josephine Closan <89244648+JoeClos@users.noreply.github.com>
2023-04-08 16:58:13 +0300

    install react icons and implement sorting for journeys

--- hsl-client/src/components/Journeys.jsx
+++ hsl-client/src/components/Journeys.jsx
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react";
 import { api } from "../config";
 import { TablePagination } from "@mui/material";
 import Search from "./Search";
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 
 const Journeys = () => {
   const [journeys, setJourneys] = useState([]);
   const [rowsPerPage, setRowsPerPage] = useState(10);
   const [page, setPage] = useState(0);
+  const [sorted, setSorted] = useState({ sorted: "departure_station_name", reversed: false });
 
   // Journeys search
   const [search, setSearch] = useState("");
@@ -16,9 +18,31 @@ const Journeys = () => {
     setSearch(query);
   };
 
-  const filteredList = (journeys.data || []).filter((journey) =>
-    journey.departure_station_name.toLowerCase().includes(search.toLowerCase())  
+  const filteredList = journeys.filter((journey) =>
+    journey.departure_station_name.toLowerCase().includes(search.toLowerCase())
   );
+
+  // Sorting journeys
+  const sortByName = () => {
+    const sortedJourneys = [...journeys];
+    sortedJourneys.sort((a, b) => {
+      const departureA = `${a.departure_station_name}`;
+      const departureB = `${b.departure_station_name}`;
+      if (sorted.reversed) {
+        return departureB.localeCompare(departureA);
+      }
+      return departureA.localeCompare(departureB);
+    });
+    setJourneys(sortedJourneys);
+    setSorted({ sorted: "departure_station_name", reversed: !sorted.reversed });
+  };
+
+  const renderArrow = () => {
+    if (sorted.reversed) {
+      return <FaArrowUp />;
+    }
+    return <FaArrowDown />;
+  };
 
   // Pagination
   const handleChangePage = (_event, newPage) => {
@@ -35,8 +59,8 @@ const Journeys = () => {
     axios
       .get(getJourneys)
       .then((response) => {
-        setJourneys(response);
-        console.log(response);
+        setJourneys(response.data);
+        console.log(response.data);
       })
       .catch((err) => {
         console.log(err);
@@ -49,23 +73,36 @@ const Journeys = () => {
       <TablePagination
         rowsPerPageOptions={[10, 25, 50, 100]}
         component="div"
-        count={(journeys.data || []).length}
+        count={journeys.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
         onRowsPerPageChange={handleChangeRowsPerPage}
       />
       <table>
-        <tbody>
+        <thead>
           <tr>
-            <th>Departure</th>
+            <th onClick={sortByName}>
+              <span style={{ marginRight: 10 }}>Departure</span>
+              {sorted.sorted === "departure_station_name" ? renderArrow() : null}
+            </th>
             <th>Return</th>
             <th>Covered distance (km)</th>
             <th>Duration (min)</th>
           </tr>
+        </thead>
+        <tbody>
           {filteredList &&
             filteredList
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .sort((a,b) => {
+                if(a.departure_station_name.toLowerCase() < b.departure_station_name.toLowerCase()
+                )return -1;
+                if(a.departure_station_name.toLowerCase() > b.departure_station_name.toLowerCase()
+                )return 1;
+                return 0;
+
+              })
               .map((journey) => (
                 <tr key={journey._id}>
                   <td>{journey.departure_station_name}</td>
