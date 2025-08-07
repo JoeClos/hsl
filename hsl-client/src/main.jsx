b7ee13777e61f1ba5dfec56e5ed9788c8db4ac7a
Josephine Closan <89244648+JoeClos@users.noreply.github.com>
2025-08-07 15:24:26 +0300

    migrate frontend to react+vite and fix the node version

---
+++ hsl-client/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'leaflet/dist/leaflet.css';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
